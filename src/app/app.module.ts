aea6183ac4bc9497208997c0c6914cc5ebb33d83
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2023-04-17 21:15:22 +0200

    Added folder structure and components manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from "./recipe/recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    [RecipeComponent, RecipeItemComponent, RecipeDetailComponent],
+    [ShoppingListComponent, ShoppingListEditComponent]
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+  templateUrl: './recipe-detail.component.html',
+  selector: 'recipe-detail',
+})
+export class RecipeDetailComponent{}
