27a22e6e9151a841f056c626014090638fa2ed58
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2023-04-27 23:41:51 +0200

    added event and property binding for shopping-list component

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   templateUrl: './shopping-list.component.html',
   selector: 'shopping-list-edit'
 })
-export class ShoppingListEditComponent{}
+export class ShoppingListEditComponent implements OnInit{
+  // @ts-ignore
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  // @ts-ignore
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
+}
