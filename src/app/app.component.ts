04d5ce021b2e598e9d69101b7ae379c5616ab4a5
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2023-04-27 23:39:24 +0200

    added event and property binding for recipe components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'recipes-cart';
+  loadedFeature: string = 'recipe';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature
+  }
 }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,11 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {Recipe} from "../recipe.model";
 
 @Component({
   templateUrl: './recipe-detail.component.html',
   selector: 'recipes-detail',
 })
-export class RecipeDetailComponent{}
+export class RecipeDetailComponent{
+  // @ts-ignore
+ @Input() recipe: Recipe;
+}
