1627dc79caf611687a0b2ee6ff244f46d00fe3d2
Gideon Daniels <gideon.daniels@safetyio.com>
2024-04-20 19:36:05 +0200

    Add tests for recipe-edit.component

--- src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.spec.ts
@@ -1,14 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeEditComponent } from './recipe-edit.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterTestingModule} from "@angular/router/testing";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RecipeService} from "../recipe.service";
 
-describe('RecipeEditComponent', () => {
+fdescribe('RecipeEditComponent', () => {
   let component: RecipeEditComponent;
   let fixture: ComponentFixture<RecipeEditComponent>;
 
+  class FakeRecipeService{
+    constructor() {
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecipeEditComponent ]
+      declarations: [ RecipeEditComponent ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+        FormsModule,
+        BrowserDynamicTestingModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: RecipeService, useClass: FakeRecipeService },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
 
