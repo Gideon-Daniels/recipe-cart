d800a87066b3c327550aa7c8a8b73e2af31560d4
Gideon Daniels <gideon.daniels@safetyio.com>
2024-05-13 19:58:29 +0200

    added error handling logic in the service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,45 +1,52 @@
-import {Component} from "@angular/core";
-import {NgForm} from "@angular/forms";
-import {AuthService} from "./auth.service";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent{
+export class AuthComponent {
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService) {}
 
-  }
-
-  onSwitchMode(){
-    this.isLoginMode = !this.isLoginMode
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(authForm: NgForm) {
-    if(!authForm.valid){
-      return
+    if (!authForm.valid) {
+      return;
     }
 
     const email = authForm.value.email;
     const password = authForm.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
-    if(this.isLoginMode){
-      //...
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signUp(email, password).subscribe(resData => {
-        console.log('resData', resData);
-        this.isLoading = true;
-      }, error => {
-        console.log('error', error);
-        this.error = 'An error occurred!';
-        this.isLoading = false;
-      });
+      authObs = this.authService.signUp(email, password);
     }
+
+    authObs.subscribe(
+      (resData) => {
+        console.log('resData', resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      },
+    );
 
     authForm.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,29 +1,65 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
-  kind: string,
-  idToken: string,
-  email: string,
-  refreshToken: string,
-  expiresIn: string,
-  localId: string,
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
-@Injectable({providedIn: 'root'})
-export class AuthService{
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-
+  signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBPr-HlAWXwxHd9WSfG8TIfDbSOq3hg2ZM',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        },
+      )
+      .pipe(catchError(this.#handleError));
   }
 
-
-  signUp(email: string, password: string){
-    return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBPr-HlAWXwxHd9WSfG8TIfDbSOq3hg2ZM", {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBPr-HlAWXwxHd9WSfG8TIfDbSOq3hg2ZM',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        },
+      )
+      .pipe(catchError(this.#handleError));
   }
 
+  #handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'unknown error occurred';
+
+    if (!errorRes.error && !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+
+    return throwError(errorMessage);
+  }
 }
