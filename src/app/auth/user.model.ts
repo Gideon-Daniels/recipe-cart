749d6420ba99bfaacddd88a0854be6b02229b95d
Gideon Daniels <gideon.daniels@safetyio.com>
2024-06-10 16:37:12 +0200

    creating and storing user data

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,15 @@
+export class User {
+  constructor(
+    public email: string,
+    public userId: string,
+    private _token: string,
+    private _tokenExpirationDate: Date,
+  ) {}
+
+  get token() {
+    if (!this._tokenExpirationDate || this._tokenExpirationDate < new Date()) {
+      return null;
+    }
+    return this._token;
+  }
+}
