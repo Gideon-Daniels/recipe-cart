7097a715ee39c8451472b3c2f3c5af501e55c455
Gideon Daniels <gideon.daniels@safetyio.com>
2024-08-03 22:07:50 +0200

    added todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//to-do: add lazy loading
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
